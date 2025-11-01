d6174f5656579347d095b1b1e76a29d3d82cdff6
Agathonejriii <agathonek@gmail.com>
2025-11-01 16:01:42 +0300

    Fix infinite redirect by removing catch-all pattern

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,39 +1,24 @@
-"""
-mysite URL Configuration - Serving React Frontend + Django Backend
-"""
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
-from django.conf import settings
+from django.urls import path, include
 from django.http import HttpResponse
-from django.conf.urls.static import static
-from django.views.static import serve as static_serve
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-# Simple homepage view
 def home(request):
     return HttpResponse("""
     <h1>Student Portal API</h1>
-    <p>Welcome to the Student Portal API</p>
+    <p>Welcome to Student Portal API</p>
     <ul>
         <li><a href="/admin/">Admin Panel</a></li>
         <li><a href="/api/">API</a></li>
         <li><a href="/api/token/">Get JWT Token</a></li>
-        <li><a href="/api/accounts/">Accounts API</a></li>
-        <li><a href="/api/students/">Students API</a></li>
     </ul>
     """)
 
 urlpatterns = [
-    path('', home, name='home'),  # Simple homepage
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/accounts/', include('accounts.urls')),
     path('api/', include('students.urls')),
-]
-
-# Serve static files
-urlpatterns += [
-    path('static/<path:path>', serve, {'document_root': settings.STATIC_ROOT}),
 ]
